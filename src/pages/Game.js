ce3127ef7f071e2581a7c288a36c1dcda6dcd963
leandro <leandrochagask@gmail.com>
2022-01-16 15:21:03 -0300

    fix(Game.js): separa logica em componentes menores

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import QuestionDisplay from '../components/QuestionDisplay';
 import { getQuestions } from '../services/triviaAPI';
-
-const numberOfQuestions = 4;
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       results: [],
-      questionPosition: 0,
     };
 
     this.getQuestionsApi = this.getQuestionsApi.bind(this);
-    this.nestQuestion = this.nestQuestion.bind(this);
   }
 
   componentDidMount() {
@@ -20,56 +17,16 @@ class Game extends React.Component {
   }
 
   async getQuestionsApi() {
-    const { response_code: responseCode, results } = await getQuestions();
-    console.log('responseCode: ', responseCode);
+    const { results } = await getQuestions();
     this.setState({ results });
   }
 
-  nestQuestion() {
-    const { questionPosition } = this.state;
-    this.setState({
-      questionPosition:
-      (questionPosition < numberOfQuestions) ? questionPosition + 1 : numberOfQuestions,
-    });
-  }
-
   render() {
-    const { results, questionPosition } = this.state;
+    const { results } = this.state;
 
     return (
       (results.length > 0)
-        ? (
-          <div>
-            <div data-testid="question-category">
-              { results[questionPosition].category }
-            </div>
-            <div data-testid="question-text">{ results[questionPosition].question }</div>
-            <div data-testid="answer-options">
-              <button type="button" data-testid="correct-answer">
-                { results[questionPosition].correct_answer }
-              </button>
-              <div>
-                {
-                  results[questionPosition].incorrect_answers.map((index) => (
-                    <button
-                      type="button"
-                      data-testid={ `wrong-answer-${index}` }
-                      key={ index }
-                    >
-                      { index }
-                    </button>))
-                }
-              </div>
-            </div>
-            <button
-              type="button"
-              onClick={ () => this.nestQuestion() }
-            >
-              Próxima
-            </button>
-          </div>
-        )
-        : (<div>Carregando</div>)
+        ? (<QuestionDisplay results={ results } />) : (<div>Carregando</div>)
     );
   }
 }
