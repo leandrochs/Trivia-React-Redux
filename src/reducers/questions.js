3b1362cee8556690a80933cf7e474681eb1ee1e0
leandro <leandrochagask@gmail.com>
2022-01-16 20:10:22 -0300

    feat(reducers/questions.js): adiciona reducer para array de questoes

---
+++ src/reducers/questions.js
@@ -0,0 +1,18 @@
+import { QUESTIONS_RESPONSE } from '../actions/questions';
+
+const INITIAL_STATE = {
+  questions: [],
+};
+
+const questionsReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case QUESTIONS_RESPONSE:
+    return {
+      ...state,
+      questions: payload,
+    };
+  default: return state;
+  }
+};
+
+export default questionsReducer;
