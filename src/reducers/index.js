5da1f3aba19f035450d70f6831b916f667f9e352
leandro <leandrochagask@gmail.com>
2022-01-16 20:02:33 -0300

    feat(reducers/index.js): adiciona reducer para questoes e posicao

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import playerReducer from './player';
+import positionReducer from './questionPosition';
+import questionsReducer from './questions';
 import tokenReducer from './token';
 
 const rootReducer = combineReducers({
   player: playerReducer,
   token: tokenReducer,
+  responseApi: questionsReducer,
+  position: positionReducer,
 });
 
 export default rootReducer;
