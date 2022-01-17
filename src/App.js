7cc77ef31dd13655dca6a2e94b5eade14506fdf6
Flavio Alves <flavio.alves84@gmail.com>
2022-01-17 19:16:59 -0300

    acerto req12

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
+      <Route path="/feedback" component={ Feedback } />
     </Switch>
   );
 }
