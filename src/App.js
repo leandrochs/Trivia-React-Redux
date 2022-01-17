56da2b0a15bd1cdfef4a87dd96f25b928bc986a6
leandro <leandrochagask@gmail.com>
2022-01-17 20:02:21 -0300

    feat(App.js): adiciona Route para configuration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
+import Configuration from './pages/Configuration';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
       <Route path="/feedback" component={ Feedback } />
+      <Route path="/configuration" component={ Configuration } />
     </Switch>
   );
 }
