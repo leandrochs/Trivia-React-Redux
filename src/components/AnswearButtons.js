b31c08291470cd87f3038c8b997e010b8f7eb92f
leandro <leandrochagask@gmail.com>
2022-01-17 15:53:36 -0300

    feat(AnswearButtons.js): adiciona logica para diferenciar respostas corretas ou incorretas

--- src/components/AnswearButtons.js
+++ src/components/AnswearButtons.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { showNextButton } from '../actions/showNextButton';
 
 const RAN_INT = 100;
 
@@ -11,17 +13,41 @@ class AnswearButtons extends React.Component {
     };
 
     this.randomAnswers = this.randomAnswers.bind(this);
+    this.printBorder = this.printBorder.bind(this);
     this.onClick = this.onClick.bind(this);
+    this.onClick2 = this.onClick2.bind(this);
   }
 
   componentDidMount() {
     this.randomAnswers();
   }
 
+  componentDidUpdate(prevProps) {
+    const { result } = this.props;
+    if (prevProps.result !== result) {
+      this.randomAnswers();
+    }
+  }
+
   onClick() {
+    this.printBorder();
+
+    console.log('certo');
+  }
+
+  onClick2() {
+    this.printBorder();
+
+    console.log('errado');
+  }
+
+  printBorder() {
     document.querySelector('.correct-answer').style.border = '3px solid rgb(6, 240, 15)';
     document.querySelectorAll('.wrong-answer')
       .forEach((button) => { button.style.border = '3px solid rgb(255, 0, 0)'; });
+
+    const { dispatch } = this.props;
+    dispatch(showNextButton(true));
   }
 
   randomAnswers() {
@@ -42,7 +68,7 @@ class AnswearButtons extends React.Component {
           key={ index }
           className={ (index === 0) ? 'correct-answer bt' : 'wrong-answer bt' }
           data-testid={ (index === 0) ? 'correct-answer' : `wrong-answer-${index - 1}` }
-          onClick={ this.onClick }
+          onClick={ (index === 0) ? this.onClick : this.onClick2 }
         >
           { answer }
         </button>),
@@ -70,4 +96,4 @@ AnswearButtons.propTypes = {
   results: PropTypes.arrayOf(PropTypes.object),
 }.isRequired;
 
-export default AnswearButtons;
+export default connect()(AnswearButtons);
