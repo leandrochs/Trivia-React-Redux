81660f42603cd34ef2c36d51389ff0d9608ded55
leandro <leandrochagask@gmail.com>
2022-01-18 16:25:38 -0300

    feat(App.js): adiona rota para ranking

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
 import Configuration from './pages/Configuration';
+import Ranking from './pages/ranking';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
       <Route path="/feedback" component={ Feedback } />
+      <Route path="/ranking" component={ Ranking } />
       <Route path="/configuration" component={ Configuration } />
     </Switch>
   );
